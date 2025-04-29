a6242fff5883acaa5e8a164e7c32a91144db1653
TX-Phuc-d8m7 <tx.phuc.dev@gmail.com>
2025-04-29 10:18:39 +0700

    Module 1 & 2

---
+++ Module 2 - Building Blocks/script.js
@@ -0,0 +1,17 @@
+// Variable Naming Best Practices
+
+// Allowed & Best Practice
+let user;
+let firstName; // Camel Case
+let result; // Choose the meaninful name that clearly describe what the variable is about
+let firstName2;
+// let name = 'John';
+// let age = 20;
+
+// Not Allowed / Not Recommended
+var apple;
+let firstname; // Snake Case
+let a; // Not clear
+// let 2firstName; // Cannot start variable name with a number
+// let let; - Can not use keyword to named a variable
+let name = 'John', age = 20;
